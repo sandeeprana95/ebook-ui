4e2361ae7c2a2e7b986035986fdde72a92209267
sandeeprana95 <sandeeprana385@gmail.com>
2025-07-02 19:56:59 +0530

    add phone input

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,12 @@
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
 import { useState } from "react"
 import { Link } from "react-router-dom"
 
-const Signup=()=>{
 
+const Signup=()=>{
   const [type,setType]=useState("password")
+  const [mobile,setMobile]=useState("")
 
     const handleInput=(e)=>{
         const input = e.target
@@ -55,6 +58,18 @@ const Signup=()=>{
                          </button>
                     </div>
                 </div>
+                <div className="flex flex-col gap-2">
+                    <label className="font-medium">Phone</label>
+                  <PhoneInput
+                    country={'in'}
+                    name="mobile"
+                    value={mobile}
+                    onChange={(value)=>setMobile(value)}
+                    inputStyle={{width:"100%",height:"48.6px",border:"1px solid gray"}}
+                    enableSearch
+                    />
+                </div>
+                
                 <div>
                     <button className="w-full bg-blue-500 text-white py-3 rounded hover:bg-blue-600" >Signup</button>
                 </div>
